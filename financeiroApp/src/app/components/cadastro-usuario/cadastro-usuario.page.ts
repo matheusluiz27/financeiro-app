84fbcadd8cd6822068d003ea0f697a9cff56b60d
matheus luiz <matheusluiz27@gmail.com>
2019-11-28 04:00:09 -0300

    fazendo requisições para cadasro de usuario

--- financeiroApp/src/app/components/cadastro-usuario/cadastro-usuario.page.ts
+++ financeiroApp/src/app/components/cadastro-usuario/cadastro-usuario.page.ts
@@ -1,3 +1,6 @@
+import { ToastrService } from 'ngx-toastr';
+import { UsuarioServiceService } from './../../services/usuario-service/usuario-service.service';
+import { Login } from './../../models/Login';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,13 +11,27 @@ import { Router } from '@angular/router';
 })
 export class CadastroUsuarioPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private usuarioService: UsuarioServiceService, private toastr: ToastrService) { }
 
   ngOnInit() {
   }
 
-  cadastrar() {
-    this.router.navigate(['/login'])
-  }
+  private usuario = new Login();
+  private senhaConfirmada: String;
 
+  cadastrar() {
+    if(this.usuario.senha != this.senhaConfirmada) {
+      this.toastr.error("Confirmação da senha inválida")
+      return
+    }
+
+    const response = this.usuarioService.cadastrarUsuario(this.usuario)
+    response.then(usuario => {
+      this.toastr.success("Usuario cadastrado com sucesso!")
+      this.router.navigate(['/login'])
+    }).catch(erro => {
+      this.toastr.error("Não foi possível cadastrar o usuario")
+    })
+  }
 }
+
