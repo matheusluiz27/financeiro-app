d2e34fbbf3c7ae1392b072b3ac4fa39a7280a8f8
matheus luiz <matheusluiz27@gmail.com>
2019-11-28 04:00:55 -0300

    fazendo requisição de login

--- financeiroApp/src/app/components/login/login.page.ts
+++ financeiroApp/src/app/components/login/login.page.ts
@@ -1,5 +1,8 @@
+import { Login } from './../../models/Login';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/services/auth-service/auth-service.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +11,20 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthServiceService, private toastr: ToastrService) { }
 
   ngOnInit() {
   }
 
+  private login = new Login();
+
   logar() {
-    this.router.navigate(['/home'])
+      const response = this.authService.login(this.login)
+      response.then(token => {
+        localStorage.setItem('token', JSON.stringify(token['token']))
+        this.router.navigate(['/home'])
+      }).catch(erro => {
+        this.toastr.error("Login Inválido")
+      })
   }
 }
