b992da1f373e96b9e504c025357ad96142cea8b8
matheus luiz <matheusluiz27@gmail.com>
2019-11-28 04:00:28 -0300

    finalizando pagina home

--- financeiroApp/src/app/components/home/home.page.ts
+++ financeiroApp/src/app/components/home/home.page.ts
@@ -1,38 +1,53 @@
+import { CaixaInfoServiceService } from './../../services/caixa-info-service/caixa-info-service.service';
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 import * as Chart from 'chart.js';
+import { CaixaInfo } from 'src/app/models/CaixaInfo';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(private elementRef: ElementRef) { }
+  ngOnInit() {
+    this.chart();
+    this.getCaixaInfo();
+  }
+
+  constructor(private elementRef: ElementRef, private caixaInfoService: CaixaInfoServiceService) { }
 
   public myChart: any;
 
+  private caixaInfo = new CaixaInfo();
+
   OnInit() {
-    this.chart
+  }
+
+  getCaixaInfo() {
+    const response = this.caixaInfoService.getCaixaInfo();
+    response.then(caixa => {
+      this.caixaInfo = caixa;
+    })
   }
 
   chart() {
-
     var htmlRef = this.elementRef.nativeElement.querySelector(`#grafico`);
     this.myChart = new Chart(htmlRef, {
-      type: 'bar',
-
+      type: 'doughnut',
       data: {
         datasets: [{
-          barPercentage: 0.5,
-          barThickness: 6,
-          maxBarThickness: 8,
-          minBarLength: 2,
-          data: [10, 20, 30, 40, 50, 60, 70]
-        }]
-      },
+          data: [50, 30],
+          backgroundColor: [ '#ff6384', '#36a2eb']
+        }],
+
+        labels: [
+          'Entradas',
+          'Saídas'
+        ]
+      }
     });
   }
 
