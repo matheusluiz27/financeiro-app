aab218663986a7e4f990e54fd792797874c045cc
matheus luiz <matheusluiz27@gmail.com>
2019-11-28 04:01:25 -0300

    fazendo requisição para movimentações

--- financeiroApp/src/app/components/movimentacoes/movimentacoes.page.ts
+++ financeiroApp/src/app/components/movimentacoes/movimentacoes.page.ts
@@ -1,4 +1,8 @@
+import { CaixaInfoServiceService } from './../../services/caixa-info-service/caixa-info-service.service';
+import { MovimentacaoServiceService } from './../../services/movimentacao-service/movimentacao-service.service';
+import { Movimentacao } from './../../models/Movimentacao';
 import { Component, OnInit } from '@angular/core';
+import { CaixaInfo } from 'src/app/models/CaixaInfo';
 
 @Component({
   selector: 'app-movimentacoes',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovimentacoesPage implements OnInit {
 
-  constructor() { }
+  constructor(private movimentacaoService: MovimentacaoServiceService, private caixaInfoServiceService: CaixaInfoServiceService) { }
 
   ngOnInit() {
+    this.getCaixaInfoPorMes(new Date);
+    this.getMovimentacoes(new Date)
+  }
+
+  private movimentacoes =  Array<Movimentacao>();
+
+  private caixaInfo = new CaixaInfo();
+
+  private data: any;
+
+  getMovimentacoes(data) {
+    const response = this.movimentacaoService.buscarMovimentacaoPorMes(data);
+    response.then(movimentacoes => {
+      this.movimentacoes = movimentacoes;
+    })
+  }
+
+  getCaixaInfoPorMes(data) {
+    const response = this.caixaInfoServiceService.getCaixaInfoPorMes(data);
+    response.then(caixaInfo => {
+      this.caixaInfo = caixaInfo;
+    })
+  }
+
+  buscarPorMes(){
+    this.getCaixaInfoPorMes(this.data);
+    this.getMovimentacoes(this.data);
   }
 
 }
